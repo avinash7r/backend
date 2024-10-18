b4a66199d0248434cf7904cfb95821356a38f896
Avinash <rajureavinash7@gmail.com>
2024-10-19 00:26:05 +0530

    add delete function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { genHash } from "../utils/hash.js";
+import { genHash, checkPassword } from "../utils/hash.js";
 import users from "../modules/users.js";
 
 export const createUser = async (req, res) => {
@@ -27,5 +27,31 @@ export const createUser = async (req, res) => {
   } catch (error) {
     console.error("error message:", error);
     return res.status(500).json({ message: error });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { mail, password } = req.body;
+  if (!mail || !password) {
+    return console.log("please provide all the fields");
+  }
+  try {
+    var curUser = await users.findOne({ mail });
+    if (!curUser) {
+      return res.status(404).json({ message: `no user with mail:${mail}` });
+    }
+    const match = await checkPassword(password, curUser.password);
+    if (!match) {
+      return res.status(400).json({ message: "check mail or password" });
+    }
+    await users.findOneAndDelete({ mail });
+    return res.status(200).json({
+      success: true,
+      message: "user was deleted",
+      data: { name: curUser.name, mail },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "internal error", error });
   }
 };
